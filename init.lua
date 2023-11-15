250a5a5d65a75231f03ea315b93bc07951c40bc8
maxwelbm <maxwelbm@hotmail.com>
2023-11-15 09:25:39 -0300

    simplifying my configuration style

--- init.lua
+++ init.lua
@@ -176,7 +176,7 @@ require('lazy').setup({
     { 'akinsho/toggleterm.nvim' },
     { 'nvim-tree/nvim-web-devicons' },
     { 'terrortylor/nvim-comment' },
-    { 'crispgm/nvim-go' },
+    { 'maxwelbm/gonv' },
     { 'doums/darcula' },
     { 'andweeb/presence.nvim' },
 }, {})
@@ -189,7 +189,7 @@ require('lazy').setup({
 vim.o.hlsearch = false
 
 -- Make line numbers default
-vim.wo.relativenumber = true
+-- vim.wo.relativenumber = true
 vim.wo.number = true
 
 -- local columns = { 80, 120 } -- Coloque os números das colunas limite desejadas aqui
@@ -512,29 +512,6 @@ cmp.setup {
 
 require("neoscroll").setup()
 
--- require("catppuccin").setup({
---     flavour = "frappe", -- latte, frappe, macchiato, mocha
---     background = { -- :h background
---         dark = "frappe",
---     },
---     transparent_background = true, -- disables setting the background color.
---
---     color_overrides = {},
---     custom_highlights = {},
---     integrations = {
---         cmp = true,
---         gitsigns = true,
---         nvimtree = true,
---         treesitter = true,
---         notify = false,
---         mini = false,
---     },
--- })
-
--- setup must be called before loading
--- vim.cmd.colorscheme "darcula"
--- vim.cmd.colorscheme "catppuccin"
-
 require("nvim-tree").setup({
     filters = {
         dotfiles = false,
@@ -624,6 +601,7 @@ vim.keymap.set("t", "<C-h>", "<C-\\><C-N><C-w>h", { desc = "window: Focus left" 
 vim.keymap.set("t", "<C-l>", "<C-\\><C-N><C-w>l", { desc = "window: Focus right" })
 vim.keymap.set("t", "<C-j>", "<C-\\><C-N><C-w>j", { desc = "window: Focus down" })
 vim.keymap.set("t", "<C-k>", "<C-\\><C-N><C-w>k", { desc = "window: Focus up" })
+
 
 require("toggleterm").setup({
     open_mapping = [[<c-\>]],
@@ -715,7 +693,7 @@ require("presence").setup({
 require('onedark').setup {
     -- Main options --
     style = 'dark',               -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-    transparent = false,          -- Show/hide background
+    transparent = true,           -- Show/hide background
     term_colors = true,           -- Change terminal color as per the selected theme style
     ending_tildes = false,        -- Show the end-of-buffer tildes. By default they are hidden
     cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
@@ -752,232 +730,41 @@ require('onedark').setup {
     },
 }
 
--- Eviline config for lualine
--- Author: shadmansaleh
--- Credit: glepnir
-local lualine = require('lualine')
-
--- Color table for highlights
--- stylua: ignore
-local colors = {
-    bg       = '#202328',
-    fg       = '#bbc2cf',
-    yellow   = '#ECBE7B',
-    cyan     = '#008080',
-    darkblue = '#081633',
-    green    = '#98be65',
-    orange   = '#FF8800',
-    violet   = '#a9a1e1',
-    magenta  = '#c678dd',
-    blue     = '#51afef',
-    red      = '#ec5f67',
-}
-
-local conditions = {
-    buffer_not_empty = function()
-        return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
-    end,
-    hide_in_width = function()
-        return vim.fn.winwidth(0) > 80
-    end,
-    check_git_workspace = function()
-        local filepath = vim.fn.expand('%:p:h')
-        local gitdir = vim.fn.finddir('.git', filepath .. ';')
-        return gitdir and #gitdir > 0 and #gitdir < #filepath
-    end,
-}
-
--- Config
-local config = {
-    options = {
-        transparent = true,
-        -- Disable sections and component separators
-        component_separators = '',
-        section_separators = '',
-        theme = {
-            -- We are going to use lualine_c an lualine_x as left and
-            -- right section. Both are highlighted by c theme .  So we
-            -- are just setting default looks o statusline
-            normal = { c = { fg = colors.fg } },
-            inactive = { c = { fg = colors.fg } },
-        },
-    },
-    sections = {
-        -- these are to remove the defaults
-        lualine_a = {},
-        lualine_b = {},
-        lualine_y = {},
-        lualine_z = {},
-        -- These will be filled later
-        lualine_c = {},
-        lualine_x = {},
-    },
-    inactive_sections = {
-        -- these are to remove the defaults
-        lualine_a = {},
-        lualine_b = {},
-        lualine_y = {},
-        lualine_z = {},
-        lualine_c = {},
-        lualine_x = {},
-    },
-}
-
--- Inserts a component in lualine_c at left section
-local function ins_left(component)
-    table.insert(config.sections.lualine_c, component)
-end
-
--- Inserts a component in lualine_x at right section
-local function ins_right(component)
-    table.insert(config.sections.lualine_x, component)
-end
-
--- ins_left {
---     function()
---         return '▊'
---     end,
---     color = { fg = colors.blue },      -- Sets highlighting of component
---     padding = { left = 0, right = 1 }, -- We don't need space before this
--- }
-
-ins_left {
-    -- mode component
-    function()
-        -- return '▊ 󰌌'
-        return ' 󰌌'
-        -- return '▊ '
-    end,
-    color = function()
-        -- auto change color according to neovims mode
-        local mode_color = {
-            n = colors.red,
-            i = colors.green,
-            v = colors.blue,
-            [''] = colors.blue,
-            V = colors.blue,
-            c = colors.magenta,
-            no = colors.red,
-            s = colors.orange,
-            S = colors.orange,
-            [''] = colors.orange,
-            ic = colors.yellow,
-            R = colors.violet,
-            Rv = colors.violet,
-            cv = colors.red,
-            ce = colors.red,
-            r = colors.cyan,
-            rm = colors.cyan,
-            ['r?'] = colors.cyan,
-            ['!'] = colors.red,
-            t = colors.red,
-        }
-        return { fg = mode_color[vim.fn.mode()] }
-    end,
-    padding = { right = 1 },
-}
-
-ins_left {
-    'filename',
-    cond = conditions.buffer_not_empty,
-    color = { fg = colors.magenta, gui = 'bold' },
-}
-
-ins_left {
-    'branch',
-    icon = '',
-    color = { fg = colors.violet, gui = 'bold' },
-}
-
-ins_left {
-    'diff',
-    -- Is it me or the symbol for modified us really weird
-    symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
-    diff_color = {
-        added = { fg = colors.green },
-        modified = { fg = colors.orange },
-        removed = { fg = colors.red },
-    },
-    cond = conditions.hide_in_width,
-}
-
--- Insert mid section. You can make any number of sections in neovim :)
--- for lualine it's any number greater then 2
-ins_left {
-    function()
-        return '%='
-    end,
-}
-
-ins_right {
-    -- Lsp server name .
-    function()
-        local msg = 'No Active Lsp'
-        local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-        local clients = vim.lsp.get_active_clients()
-        if next(clients) == nil then
-            return msg
-        end
-        for _, client in ipairs(clients) do
-            local filetypes = client.config.filetypes
-            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                return client.name
-            end
-        end
-        return msg
-    end,
-    icon = '  LSP:',
-    color = { fg = '#f4f4f4', gui = 'bold' },
-}
-
-ins_right {
-    'fileformat',
-    fmt = string.upper,
-    icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
-    color = { fg = colors.green, gui = 'bold' },
-}
-
--- Add components to right sections
-ins_right {
-    'o:encoding',       -- option component same as &encoding in viml
-    fmt = string.upper, -- I'm not sure why it's upper case either ;)
-    cond = conditions.hide_in_width,
-    color = { fg = colors.green, gui = 'bold' },
-}
-
-ins_right {
-    -- filesize component
-    'filesize',
-    cond = conditions.buffer_not_empty,
-}
-
-ins_right { 'location' }
-
-ins_right { 'progress', color = { fg = colors.fg, gui = 'bold' } }
-
-ins_right {
-    'diagnostics',
-    sources = { 'nvim_diagnostic' },
-    symbols = { error = ' ', warn = ' ', info = ' ' },
-    diagnostics_color = {
-        color_error = { fg = colors.red },
-        color_warn = { fg = colors.yellow },
-        color_info = { fg = colors.cyan },
-    },
-}
-
--- Now don't forget to initialize lualine
-lualine.setup(config)
-
 require('onedark').load()
 
+-- require("catppuccin").setup({
+--     flavour = "frappe", -- latte, frappe, macchiato, mocha
+--     background = { -- :h background
+--         dark = "frappe",
+--     },
+--     transparent_background = true, -- disables setting the background color.
+--
+--     color_overrides = {},
+--     custom_highlights = {},
+--     integrations = {
+--         cmp = true,
+--         gitsigns = true,
+--         nvimtree = true,
+--         treesitter = true,
+--         notify = false,
+--         mini = false,
+--     },
+-- })
+
+-- setup must be called before loading
+-- vim.cmd.colorscheme "darcula"
+-- vim.cmd.colorscheme "catppuccin"
 
 -- vim.o.cursorcolumn = true
 -- vim.o.cursorline = true
-vim.o.laststatus = 3
 -- vim.o.linebreak = true
 -- vim.o.wrap = true
 vim.cmd("set nowrap")
 
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+vim.keymap.set("n", "<leader>df", "<cmd>vertical Git diff %<cr>", { desc = '[space|df] opens a git diff vertically' })
+
+vim.o.laststatus = 0
