e145a17c4ab39f9128539004eb39175d87931881
maxwelbm <maxwelbm@hotmail.com>
2023-11-20 21:14:08 -0300

    chore: cleaning the house

--- init.lua
+++ init.lua
@@ -160,8 +160,8 @@ require('lazy').setup({
     -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
     --       These are some example plugins that I've included in the kickstart repository.
     --       Uncomment any of the lines below to enable them.
-    require 'nvim.plugins.autoformat',
-    require 'nvim.plugins.debug',
+    -- require 'nvim.plugins.autoformat',
+    -- require 'nvim.plugins.debug',
 
     -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
     --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
