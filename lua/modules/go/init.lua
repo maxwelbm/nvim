76cce8beb20ffdb2940beefeee673bb83732dae9
maxwelbm <maxwelbm@hotmail.com>
2024-08-24 01:11:34 -0300

    feat: auto GoFormat to files .go

--- lua/modules/go/init.lua
+++ lua/modules/go/init.lua
@@ -8,3 +8,12 @@ require('modules.go.cmds.import')
 require('modules.go.cmds.quick_type')
 require('modules.go.cmds.struct_tags')
 require('modules.go.cmds.install')
+
+-- Adiciona um autocmd que executa um command toda vez que um arquivo Go é salvo
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.go",
+  callback = function()
+    -- O command que você deseja executar
+    vim.cmd("GoFormat")
+  end,
+})
